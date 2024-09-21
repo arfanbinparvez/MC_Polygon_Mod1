b0f109d0622fe123ea6b91d3e1f625598df4b5a9
arfanbinparvez <123824765+arfanbinparvez@users.noreply.github.com>
2024-09-21 13:58:12 +0530

    Update approveDeposit.js

--- approveDeposit.js
+++ approveDeposit.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 const fxRootContractABI = require("../fxRootContractABI.json");
 const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
 
-const tokenAddress = "0x7f3838051489595Aec7981fC13493752c99db792"; 
+const tokenAddress = "0xdb7B94aeAEf9d4BcAA1b1A8ce2D4aA06982ae43b"; 
 const tokenABI = tokenContractJSON.abi;
 const fxERC20RootAddress = "0x3658ccFDE5e9629b0805EB06AaCFc42416850961";
 const walletAddress = "0xEbD04f411D295086497E4686b15D7c9d87ad434D"; 
